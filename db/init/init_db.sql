c9616748b78894ec999ed6c55cc7a71cc688a541
nas <nas.silver.bullet@gmail.com>
2020-01-27 23:24:44 +0900

    :+1: Add users table config

--- db/init/init_db.sql
+++ db/init/init_db.sql
@@ -1,1 +1,10 @@
 CREATE DATABASE test_db character SET utf8mb4 collate utf8mb4_bin;
+
+USE test_db;
+
+CREATE TABLE users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  age int(11) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
